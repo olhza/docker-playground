3bef3dbc8af0f3b8a1a619f58c3a7d29fa77c114
Olivier Haremza <olh@oharemza.com>
2016-02-04 19:34:55 +0100

    Initial commit

---
+++ src/main/java/com/oharemza/timeteller/TimetellerApplication.java
@@ -0,0 +1,12 @@
+package com.oharemza.timeteller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetellerApplication.class, args);
+	}
+}
